44536aa1ae9f3e154f0471303471bb0f540c9139
zhuang007 <1757717892@qq.com>
2020-10-14 15:11:13 +0800

    1

---
+++ src/views/home/Home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="home">
+    <!--  导航  -->
+    <nav-bar class="nav">
+      <div slot="center">购物街</div>
+    </nav-bar>
+    <!--  轮播图  -->
+
+  </div>
+</template>
+
+<script>
+  import NavBar from "components/common/navbar/NavBar"
+
+  export default {
+    name: "Home",
+    components: {
+      NavBar
+    }
+  }
+</script>
+
+<style scoped>
+  .nav{
+    background: pink;
+  }
+</style>
